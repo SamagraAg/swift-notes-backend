0288df7bb45f483d9354a93b5bf4ffb07d9c506b
Samagra Agrawal <samagra110@gmail.com>
2024-03-17 22:33:46 +0530

    Add Validation checks to registration route

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../Models/User");
+const { body, validationResult } = require("express-validator");
 
 //post route for registering user(No auth required)
-router.post("/", (req, res) => {
-  res.send(req.body);
-  const user = User(req.body);
-  user.save();
-});
+router.post(
+  "/",
+  [
+    body("name", "Name cannot be empty").notEmpty(),
+    body("email", "Enter a valid email").isEmail(),
+    body(
+      "password",
+      "Password must be 8characters long. 1 lowercase, 1 uppercase, 2 number and 1 special character is required"
+    ).isStrongPassword({
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    }),
+  ],
+  (req, res) => {
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+      res.send(req.body);
+      const user = User(req.body);
+      user.save();
+    }
+    res.send({ errors: result.array() });
+  }
+);
 
 module.exports = router;
