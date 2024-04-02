256da7d08425c08f3d4ff9f8b937ae3600027638
Samagra Agrawal <samagra110@gmail.com>
2024-04-03 00:11:08 +0530

    Refractoring

--- Middlewares/fetchUser.js
+++ Middlewares/fetchUser.js
@@ -4,14 +4,14 @@ const JWT_SECRET = "07$aM@4Ra26";
 const fetchUser = (req, res, next) => {
   const token = req.header("auth-token");
   if (!token) {
-    return res.status(401).json("1st Please authenticate using proper token");
+    return res.status(401).json("Please authenticate using proper token");
   }
   try {
     const data = jwt.verify(token, JWT_SECRET);
     req.user = data.user;
   } catch(err) {
     console.log(err)
-    return res.status(401).json("2nd Please authenticate using proper token");
+    return res.status(401).json("Please authenticate using proper token");
   }
   next();
 };
