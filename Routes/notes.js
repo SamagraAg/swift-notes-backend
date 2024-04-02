9f222f0c7682b51c1333fb88db740dff0c493379
Samagra Agrawal <samagra110@gmail.com>
2024-04-02 23:23:30 +0530

    Add /fetchallnotes route for getting all user notes

--- Routes/notes.js
+++ Routes/notes.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const Note = require("../Models/Note");
+const fetchUser = require("../Middlewares/fetchUser");
 
-router.get("/", (req, res) => {
-  res.send("Notes endpoint hit");
+//Route1 : Fetch all notes: using GET "/api/notes/fetchallnotes" (Login required)
+router.get("/fetchallnotes", fetchUser, async (req, res) => {
+  try {
+    //Got user ID after jwt verification from fetchUser.js
+    const userId = req.user.id;
+
+    //Fetching Notes of that user
+    const notes = await Note.find({ user: userId }).select();
+    res.send(notes);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({ error: "Some Error Occured" });
+  }
 });
 
 module.exports = router;
