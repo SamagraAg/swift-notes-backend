d7d6ba4444594573420455eb2b5c6235c6dc0be8
Samagra Agrawal <samagra110@gmail.com>
2024-04-03 00:04:35 +0530

    Add endpoint for creating new nodes /addnote

--- Routes/notes.js
+++ Routes/notes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
 const router = express.Router();
 const Note = require("../Models/Note");
 const fetchUser = require("../Middlewares/fetchUser");
@@ -17,5 +18,52 @@ router.get("/fetchallnotes", fetchUser, async (req, res) => {
     return res.status(500).json({ error: "Some Error Occured" });
   }
 });
+
+//Route2 : Add a new note: using POST "/api/notes/addnote" (Login required)
+router.post(
+  "/addnote",
+  //handling input validations
+  [
+    body("title", "Title must be atleast 3 characters")
+      .isLength({ min: 3 })
+      .escape(),
+    body("description", "Description must be atleast 5 characters")
+      .isLength({ min: 5 })
+      .escape(),
+    body("tag").default("general").escape(),
+  ],
+  fetchUser,
+  async (req, res) => {
+    try {
+      //Return Errors if input validations doesnt pass
+      const inputErrors = validationResult(req);
+      if (!inputErrors.isEmpty()) {
+        return res.status(400).json({ errors: inputErrors.array() });
+      }
+
+      //user input for notes
+      const { title, description, tag } = req.body;
+
+      //Got user ID after jwt verification from fetchUser.js
+      const userId = req.user.id;
+
+      //Creating new note and saving in 'notes' collection
+      const newNote = await Note.create({
+        title,
+        description,
+        tag,
+        user: userId,
+      });
+
+      const savedNote = await newNote.save();
+      res.json(savedNote);
+
+      //if any server side error occurs
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).json({ error: "Some Error Occured" });
+    }
+  }
+);
 
 module.exports = router;
